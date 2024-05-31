2e6a19886e4be09997c765a208d64091493568f4
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-05-31 19:10:42 +0530

    portfolio website

---
+++ components/Header.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () =>{
+   return (
+   <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+    
+    <div className="container mx-auto">
+        <Link href={"/"}>
+        <h1 className="text-4xl font-semibold">Prathamesh<span className="text-accent">.</span></h1>
+        </Link>
+    </div>
+    
+    </header>
+    );
+};
+
+export default Header
