8050cbde9493e73a366de1a1f3612409d9f9aad5
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-06-06 19:51:10 +0530

    added the navigation and pages to the website

---
+++ app/services/page.jsx
@@ -0,0 +1,7 @@
+const Services = () => {
+    return(
+        <div>services</div>
+    );
+}
+
+export default Services
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,33 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Nav from "./Nav";
 
 const Header = () =>{
    return (
-   <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+   <header className="py-8 xl:py-12 text-white">
     
-    <div className="container mx-auto">
+    <div className="container mx-auto flex justify-between items-center">
+        {/* logo*/}
         <Link href={"/"}>
-        <h1 className="text-4xl font-semibold">Prathamesh<span className="text-accent">.</span></h1>
+        <h1 className="text-4xl font-semibold">
+            Prathamesh
+            <span className="text-accent">.</span>
+            </h1>
         </Link>
+
+
+
+        {/* desktop nav and hireme button*/}
+        <div className="hidden xl:flex items-center gap-8">
+            <Nav />
+            <Link href="/contact">
+                <Button>Hire me</Button>
+            </Link>
+        </div>
+
+        {/*mobile nav*/}
+        <div className="xl:hidden">mobile nav</div>
+
     </div>
     
     </header>
