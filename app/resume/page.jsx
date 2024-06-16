be695c233e7dd307e1bf8d5aa3b324ef6b944808
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-06-17 01:40:19 +0530

    resume page in progress

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,30 @@
+"use client";
+import {
+  FaAndroid,
+  FaJava,
+  FaFigma,
+  FaFirebase,
+  FaGit,
+  FaJira
+} from "react-icons/fa";
+
+import {
+    SiKotlin,
+    SiKtor,
+    SiFlutter,
+    SiDart,
+    SiMongodb,
+    SiMysql,
+    SiPostgresql,
+    SiPostman
+  } from "react-icons/si";
+
+  //about data
+  const about = {
+    title: "About Me",
+    description:"Quick learner and Self taught Android Developer with over 2+ years of experience in Android Development using Java, Kotlin and adept at writing high quality code for mobile application development with user-friendly user interfaces for users. Efficient with Agile development methodology for module testing and proficient in debugging and fixing errors."
+  };
+
 const Resume = () => {
     return(
         <div>Resume</div>
