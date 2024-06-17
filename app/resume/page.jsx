b55d1815e38c253e329f5ee7c7b7587e21ddf49b
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-06-17 15:50:54 +0530

    working on resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -22,7 +22,46 @@ import {
   //about data
   const about = {
     title: "About Me",
-    description:"Quick learner and Self taught Android Developer with over 2+ years of experience in Android Development using Java, Kotlin and adept at writing high quality code for mobile application development with user-friendly user interfaces for users. Efficient with Agile development methodology for module testing and proficient in debugging and fixing errors."
+    description:
+      "Quick learner and Self taught Android Developer with over 2+ years of experience in Android Development using Java, Kotlin and adept at writing high quality code for mobile application development with user-friendly user interfaces for users. Efficient with Agile development methodology for module testing and proficient in debugging and fixing errors.",
+      info: [
+        {
+          fieldName: "Name",
+          fieldValue:"Prathamesh Kumbhar"
+        },
+        {
+          fieldName: "Phone",
+          fieldValue:"+91 9082379158"
+        },
+        {
+          fieldName: "Experience",
+          fieldValue:"2+ Years"
+        },
+        {
+          fieldName: "Nationality",
+          fieldValue:"Indian"
+        },
+        {
+          fieldName: "Email",
+          fieldValue:"prathameshkumbhar291113@gmail.com"
+        },
+        {
+          fieldName: "Freelance",
+          fieldValue:"Available"
+        },
+        {
+          fieldName: "Languages",
+          fieldValue:"English, Marathi, Hindi"
+        },
+        {
+          fieldName: "Hobbies",
+          fieldValue:"Singing, Guitar, Football"
+        },
+      ]
+  };
+
+  const experience = {
+
   };
 
 const Resume = () => {
