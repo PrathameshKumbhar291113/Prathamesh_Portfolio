629c7805bc7300d2a171d733164844a9ed373029
Prathamesh Kumbhar <prathamesh.kumbhar@gd-lab.com>
2024-06-20 18:54:13 +0530

    added the code in resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -195,8 +195,20 @@ const Resume = () => {
           opacity: 1,
           transition: {delay: 2.4, duration:0.4, ease:"easeIn"},
         }}
-        className="min-h-[80vh] flex "
-        >Resume</motion.div>
+        className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+        >
+        <div className="container mx-auto">
+          <Tabs defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]">
+            <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="aboutme">About Me</TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </div>
+        Resume</motion.div>
     );
 }
 
