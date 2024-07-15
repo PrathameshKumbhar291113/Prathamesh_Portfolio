44269e2888052e5e0d34f0792caf5671c0bfec0a
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-07-15 22:42:38 +0530

    updated the social links

--- components/Social.jsx
+++ components/Social.jsx
@@ -4,7 +4,7 @@ import { FaGithub, FaLinkedinIn, FaTwitter, FaGooglePlay, FaHackerrank} from "re
 
 const socials = [
     {icon: <FaGithub/>, path: 'https://github.com/PrathameshKumbhar291113'},
-    {icon: <FaLinkedinIn/>, path: 'https://www.linkedin.com/in/prathamesh-kumbhar-1298401b6/'},
+    {icon: <FaLinkedinIn/>, path: 'https://www.linkedin.com/in/prathamesh-kumbhar-09mar2003/'},
     {icon: <FaTwitter/>, path: 'https://x.com/Pratham291113'},
     {icon: <FaHackerrank/>, path: 'https://www.hackerrank.com/profile/Pratham13'},
     {icon: <FaGooglePlay/>, path: 'https://play.google.com/store/apps/details?id=com.prathameshkumbhar.bfit&hl=en_US'},
